baebfa46a0ae3ba2d949fc9ce5706333df96cdcb
Zhang Xiaowei <weixiao08@qq.com>
2016-03-22 14:12:40 +0800

    delete rows

--- Pod/Classes/Categories/Foundation/UIDevice+XWCategory.h
+++ Pod/Classes/Categories/Foundation/UIDevice+XWCategory.h
@@ -8,25 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-//获取设备屏幕尺寸
-#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
-#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)//应用尺寸
-#define APP_WIDTH [[UIScreen mainScreen]applicationFrame].size.width
-#define APP_HEIGHT [[UIScreen mainScreen]applicationFrame].size.height
-//获取系统版本
-#define iOSVer [[[UIDevice currentDevice] systemVersion] floatValue]
-#define CurSysVer [[UIDevice currentDevice] systemVersion]
-#define isIOS4 ([[[UIDevice currentDevice] systemVersion] intValue]==4)
-#define isIOS5 ([[[UIDevice currentDevice] systemVersion] intValue]==5)
-#define isIOS6 ([[[UIDevice currentDevice] systemVersion] intValue]==6)
-#define isIOS7 ([[[UIDevice currentDevice] systemVersion] intValue]==7)
-#define isIOS8 ([[[UIDevice currentDevice] systemVersion] intValue]==8)
-#define isAfterIOS4 ([[[UIDevice currentDevice] systemVersion] intValue]>=4)
-#define isAfterIOS5 ([[[UIDevice currentDevice] systemVersion] intValue]>=5)
-#define isAfterIOS6 ([[[UIDevice currentDevice] systemVersion] intValue]>=6)
-#define isAfterIOS7 ([[[UIDevice currentDevice] systemVersion] intValue]>=7)
-#define isAfterIOS8 ([[[UIDevice currentDevice] systemVersion] intValue]>=8)
-
 @interface UIDevice (XWCategory)
 // 获取设备UUID地址，AABBCCDDEEFF
 + (NSString *)getUUIDAddress;
