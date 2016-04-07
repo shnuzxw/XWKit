b87fc37b28918943fcf08989a80486f67d168aed
Zhang Xiaowei <weixiao08@qq.com>
2016-04-07 14:27:58 +0800

    主线程中运行

--- Pod/Classes/Categories/UIKit/UIViewController+XWHUD.m
+++ Pod/Classes/Categories/UIKit/UIViewController+XWHUD.m
@@ -11,7 +11,9 @@
 @implementation UIViewController (XWHUD)
 
 - (void)xw_showLoding{
-    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    });
 }
 
 - (void)xw_showLoding:(NSString *)loding{
@@ -19,18 +21,22 @@
 }
 
 - (void)xw_showLoding:(NSString *)loding inView:(UIView *)view{
-    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-    if (view == nil) view = [UIApplication sharedApplication].keyWindow;
-    // 快速显示一个提示信息
-    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
-    hud.labelText = loding;
-    hud.removeFromSuperViewOnHide = YES;
-    // YES代表需要蒙版效果
-    hud.dimBackground = YES;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+        if (view == nil) view = [UIApplication sharedApplication].keyWindow;
+        // 快速显示一个提示信息
+        MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+        hud.labelText = loding;
+        hud.removeFromSuperViewOnHide = YES;
+        // YES代表需要蒙版效果
+        hud.dimBackground = YES;
+    });
 }
 
 - (void)xw_hideLoding{
-    [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+    });
 }
 
 - (void)xw_showMessage:(NSString *)msg{
@@ -80,17 +86,19 @@
 }
 
 - (void)xw_showImage:(UIImage *)image message:(NSString *)msg inView:(UIView *)view delay:(NSTimeInterval)delay animation:(BOOL)animation{
-    if (view == nil) view = [UIApplication sharedApplication].keyWindow;
-    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:animation];
-    if (msg.length > 10) {
-        hud.detailsLabelText = msg;
-    }else{
-        hud.labelText = msg;
-    }
-    hud.customView = [[UIImageView alloc] initWithImage:image];
-    hud.mode = MBProgressHUDModeCustomView;
-    hud.removeFromSuperViewOnHide = YES;
-    [hud hide:animation afterDelay:delay];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (view == nil) view = [UIApplication sharedApplication].keyWindow;
+        MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:animation];
+        if (msg.length > 10) {
+            hud.detailsLabelText = msg;
+        }else{
+            hud.labelText = msg;
+        }
+        hud.customView = [[UIImageView alloc] initWithImage:image];
+        hud.mode = MBProgressHUDModeCustomView;
+        hud.removeFromSuperViewOnHide = YES;
+        [hud hide:animation afterDelay:delay];
+    });
 }
 
 - (void)xw_hideHud{
@@ -107,7 +115,9 @@
 
 
 - (void)xw_hideHud:(BOOL)animated afterDelay:(NSTimeInterval)delay completeBlock:(dispatch_block_t)block{
-    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+    });
 }
 
 @end
