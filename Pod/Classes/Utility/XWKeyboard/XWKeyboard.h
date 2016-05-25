75ab674f712ba459548330a04903ec162b642c25
张校玮 <weixiao08@qq.com>
2016-05-25 16:55:31 +0800

    添加键盘管理类

---
+++ Pod/Classes/Utility/XWKeyboard/XWKeyboard.h
@@ -0,0 +1,22 @@
+//
+//  XWKeyboard.h
+//  XWKit
+//
+//  Created by smile.zhang on 16/5/25.
+//  Copyright © 2016年 Zhang Xiaowei. All rights reserved.
+//
+
+
+/**
+ * 该类希望实现如下功能:
+ * 1. 键盘弹出后在键盘上方增加一个Toolbar，有关闭键盘按钮，亦可添加自定义功能。
+ * 2. 无需编写代码，仅仅引用该类即可。
+ * 3. UITextField、UITextView、UISearchBar等控件可能需要添加类别。
+ */
+
+
+#import <Foundation/Foundation.h>
+
+@interface XWKeyboard : NSObject
+
+@end
---
+++ Pod/Classes/Utility/XWKeyboard/XWKeyboard.m
@@ -0,0 +1,13 @@
+//
+//  XWKeyboard.m
+//  XWKit
+//
+//  Created by smile.zhang on 16/5/25.
+//  Copyright © 2016年 Zhang Xiaowei. All rights reserved.
+//
+
+#import "XWKeyboard.h"
+
+@implementation XWKeyboard
+
+@end
